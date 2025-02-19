12fa008e119164278e53f8bf17335f3c497754c3
mmsqe <mavis@crypto.com>
2025-02-19 11:00:45 +0800

    skip reproduce multi fixdata

--- old/main.go
+++ old/main.go
@@ -22,30 +22,30 @@ func main() {
 
 	total := 10000
 
+	// batch := grocksdb.NewWriteBatch()
+	// for i := 0; i < total; i++ {
+	// 	key := []byte("key" + strconv.Itoa(i))
+	// 	value := []byte("value" + strconv.Itoa(i))
+	// 	var ts [tsrocksdb.TimestampSize]byte
+	// 	binary.LittleEndian.PutUint64(ts[:], uint64(1))
+	// 	batch.PutCFWithTS(cfHandle, key, ts[:], value)
+	// 	fmt.Println("wrote", string(key), string(value))
+	// }
+	// err = db.Write(defaultSyncWriteOpts, batch)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// opts := grocksdb.NewDefaultFlushOptions()
+	// defer opts.Destroy()
+
+	// err = db.FlushCF(cfHandle, opts)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// batch.Destroy()
+
 	batch := grocksdb.NewWriteBatch()
-	for i := 0; i < total; i++ {
-		key := []byte("key" + strconv.Itoa(i))
-		value := []byte("value" + strconv.Itoa(i))
-		var ts [tsrocksdb.TimestampSize]byte
-		binary.LittleEndian.PutUint64(ts[:], uint64(1))
-		batch.PutCFWithTS(cfHandle, key, ts[:], value)
-		fmt.Println("wrote", string(key), string(value))
-	}
-	err = db.Write(defaultSyncWriteOpts, batch)
-	if err != nil {
-		panic(err)
-	}
-
-	opts := grocksdb.NewDefaultFlushOptions()
-	defer opts.Destroy()
-
-	err = db.FlushCF(cfHandle, opts)
-	if err != nil {
-		panic(err)
-	}
-	batch.Destroy()
-
-	batch = grocksdb.NewWriteBatch()
 	defer batch.Destroy()
 	count := 2
 	for i := 0; i < total; i++ {
