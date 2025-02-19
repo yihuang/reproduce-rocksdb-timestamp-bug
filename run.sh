e8a2053e0779f97a676a42ace0185140821e82bd
huangyi <yi.codeplayer@gmail.com>
2025-02-19 09:55:39 +0800

    add little bit sleep

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ nix develop -c go run ./main.go /tmp/versiondb | wc -l
 cd ../new
 nix develop -c go run ./query/main.go /tmp/versiondb | wc -l
 nix develop -c go run ./fix/main.go /tmp/versiondb | wc -l
+sleep 1
 nix develop -c go run ./query/main.go /tmp/versiondb | wc -l
